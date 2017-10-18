21f7e8c6281632f06557ccc6a1a388451711d3fc
cylee1 <cylee1@s.sfusd.edu>
2017-10-18 11:35:11 -0700

    code

--- Chick.java
+++ Chick.java
@@ -2,16 +2,20 @@ class Chick implements Animal
 {     
      private String myType;     
      private String mySound;      
-     public Chick(String type, String sound)     
+     public Chick(String type, String sound, String sound1)     
      {         
          myType = type;         
-         mySound = sound;     
+         mySound = if(Math.random() <.5)
+         		return sound;
+         	else 
+         		return sound1;
+         
      }     
      public Chick()     
      {         
          myType = "unknown";         
          mySound = "unknown";     
      }      
-     public String getSound(){return mySound;}     
+     public String getSound(){return mySound;}   
      public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,7 +1,7 @@
 public void setup() 
 {     
     Cow c = new Cow("cow", "moo");   
-    Chick ch = new Chick("chick", "cluck");
+    Chick ch = new Chick("chick", getSound());
     Pig p = new Pig("pig", "oink");
     System.out.println(c.getType() + " goes " + c.getSound());
     System.out.println(ch.getType() + " goes " + ch.getSound());
